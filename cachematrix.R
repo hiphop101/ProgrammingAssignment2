6baab585020c87aa00f4bd708780e063bd961b74
hiphop101 <xufeng.chen1984@gmail.com>
2018-01-26 23:45:33 +0800

    First version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function has the getter setter methods to store and retrieve matrix and inverse matrix
+makeCacheMatrix <- function(m = matrix()) {
+    minv <- NULL
+    get <- function() m
+    getInv <- function() minv
+    set <- function (y) {
+        m <<- y
+        minv <<- NULL
+    }
+    setInv <- function(y) {
+        minv <<- y
+    }
+    list(set = set, get = get, getInv = getInv,setInv = setInv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This methods will try to retrieve inversed matrix. If it was previously cached,
+# it will return the cached copy, else from the cache.
+cacheSolve <- function(m, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    if(!is.null(m$getInv())) {
+        message("getting cached data")
+        return(m$getInv())
+    }
+    m$set(m$get())
+    
+    minv <- solve(m$get())
+    m$set(minv)
+    minv
 }
+
+#TESTING BEGIN#
+#mat <- matrix(c(5,3,4,6,7,8,1,1,2),nrow=3,ncol=3, byrow=TRUE)
+#myCacheMat <- makeCacheMatrix(mat)
+
+#myCacheMat$get()
+#cacheSolve(myCacheMat)
+#TESTING END#
